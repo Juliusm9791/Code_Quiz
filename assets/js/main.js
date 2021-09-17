5e02d7185e2d31508f1d1a6e9200f7a5f9a03ef5
Julius Markauskas <julusm9791@gmail.com>
2021-09-16 23:16:04 -0400

    added delay on answers

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,17 +9,19 @@ let answerButtons = document.querySelector("#answerButtons");
 let correctOrWrong = document.querySelector("#correctOrWrong");
 
 
+
 // Buttons
 let startButton = document.querySelector("#startButton");
 let initialsSubmitButton = document.querySelector("#initialsSubmitButton");
 let highScoreGoBackButton = document.querySelector("#highScoreGoBackButton");
-let highScoreClearButton = document.querySelector("#highScoreClearButton");
+let restartButton = document.querySelector("#restartButton");
 
 // Text inputs
 let initials = document.querySelector("#initials");
 
 let timer = 0;
-let questionTime = 50;
+let secconds = 50;
+let questionTime = secconds;
 let randomNr;
 let correctAnswer;
 
@@ -31,16 +33,27 @@ let score = {
 }
 
 let scoresArray = [];
+// getFromStorage();
+
+// function getFromStorage() {
+//     scoresArray = JSON.parse(localStorage.getItem("scoresArray"));
+//     // if (scoresArray !== null) {
+//     //     return;
+//     // document.querySelector(".message").textContent = lastGrade.student +  " received a/an " + lastGrade.grade
+//     // }
+// }
 
 
 // Quiz questions / answers
-let quizArray= [{'question': 'first', 'answers': ["one1", "two1", "three1", "fffffffffour1", 0]},
-                {'question': 'second', 'answers': ["ooooooooooone2", "two2", "three2", "four2", 1]},
-                {'question': 'third', 'answers': ["one3", "ttttttttttttwo3", "three3", "four3", 2]},
-                {'question': 'fourth', 'answers': ["one4", "two4", "tttttttttthree4", "four4", 3]},];
-           
-                
+let quizArrayOriginal= [{'question': 'first', 'answers': ["one1", "two1", "three1", "fffffffffour1", 0]},
+                        {'question': 'second', 'answers': ["ooooooooooone2", "two2", "three2", "four2", 1]},
+                        {'question': 'third', 'answers': ["one3", "ttttttttttttwo3", "three3", "four3", 2]},
+                        {'question': 'fourth', 'answers': ["one4", "two4", "tttttttttthree4", "four4", 3]},];
 
+let quizArray = [];
+quizArray.push.apply(quizArray, quizArrayOriginal);
+         
+        
 answerButtons.addEventListener("click", function(event) {
     let button = event.target;
    
@@ -48,43 +61,39 @@ answerButtons.addEventListener("click", function(event) {
             let state = button.getAttribute("data-state");
             if (state === "true") {
                 correctOrWrong.appendChild(createHTMLelement("Correct", "p"));
+                // showCorrectWrongMessage();
                 score.correctAnsw++;
-                console.log(score.correctAnsw, "corrrrr")
-                if (quizArray.length === 0) {
-                    ifNoMoreQuestions();
-                } else {
-                    startNextQuestion()
-                }
+                showCorrectWrongMessageDelay();
             } else {              
                 correctOrWrong.appendChild(createHTMLelement("Wrong", "p"));
+                // showCorrectWrongMessage();
                 questionTime -=10;
                 score.wrongAnsw++;
-                console.log(score.correctAnsw, "wrwwww")
-                if (quizArray.length === 0) {
-                    ifNoMoreQuestions();
-                } else {
-                    startNextQuestion()
-                }
+                showCorrectWrongMessageDelay();
             }
         }
   
 });
 
-  
-  
-  
-//   function getFromStorage() {
-//       let quizArrayFormMemory = JSON.parse(localStorage.getItem("quizArray"));
-//       if (quizArrayFormMemory !== null) {
-//           // document.querySelector(".message").textContent = lastGrade.student +  " received a/an " + lastGrade.grade
-//     }
-// }
-
-
 // Event Listeners
 startButton.addEventListener("click", startTimer);
 
-
+function showCorrectWrongMessageDelay() {
+      correctOrWrong.style.borderTop = "1px solid #505050";
+      if (quizArray.length === 0) {
+        setTimeout(() =>{
+            correctOrWrong.innerHTML = "";
+            correctOrWrong.style.borderTop = "0px";
+            ifNoMoreQuestions();
+        },1000);
+    } else {
+        setTimeout(() =>{
+            correctOrWrong.innerHTML = "";
+            correctOrWrong.style.borderTop = "0px";
+            startNextQuestion();
+        },1000);
+    }
+}
 function startTimer () {
     
     startNextQuestion()
@@ -112,7 +121,7 @@ function setQuestion() {
     console.log(quizArray.length, "ilgis")
     removingChildrens(questionShow);
     removingChildrens(answerButtons);
-    startInfo.style.display = "none";
+    startInfo.style.display = "block";
     allDone.style.display = "none";
     fillScore.style.display = "none";
     // questionShow.children[0].remove(); // h1 tag html TEMP
@@ -144,18 +153,19 @@ function startNextQuestion() {
 
 function writeScore () {
     let initialsSubmitButton = document.querySelector("#initialsSubmitButton");
-    initialsSubmitButton.addEventListener("click", readvalues)
-
-    function readvalues(){
+    initialsSubmitButton.addEventListener("click", function() {
         score.player = document.getElementById("initials").value;
         console.log(score.player);
+        console.log(score);
         scoresArray.push(score);
         localStorage.setItem("scoresArray", JSON.stringify(scoresArray));
-    }
+        initialsSubmitButton.display = true;
+       
+    });
 }
 
 function ifNoMoreQuestions(){
-    if (quizArray.length === 0) {
+    // if (quizArray.length === 0) {
         score.timeScore = questionTime;
         writeScore ();
         clearInterval(timer);
@@ -163,5 +173,13 @@ function ifNoMoreQuestions(){
         questionShow.style.display = "none";
         answerButtons.style.display = "none";
         correctOrWrong.style.display = "none";
-    }
+        removingChildrens(timerView);
+    // }
 }
+
+restartButton.addEventListener("click", function(){
+    quizArray = []
+    quizArray.push.apply(quizArray, quizArrayOriginal);
+    initialsSubmitButton.display = false;
+    questionTime = secconds;
+}) 
