20f477f8ef6aa6e689a306c96e7bb2ffa22ce783
Julius Markauskas <julusm9791@gmail.com>
2021-09-18 09:33:53 -0400

    small corrections

---
+++ assets/js/hihgscore.js
@@ -0,0 +1,43 @@
+let highScoreList = document.querySelector("#highScoreList");
+let highScoreGoBackButton = document.querySelector("#highScoreGoBackButton");
+let restartButton = document.querySelector("#clearButton");
+let backToMainPage = document.querySelector("#backToMainPage");
+
+let topScoreList = 5; // top ten view
+
+let scoresArray = [];
+
+scoresArray = JSON.parse(localStorage.getItem("scoresArray"));
+if (scoresArray === null) {
+    scoresArray = [];
+   }
+scoresArray.sort((a, b) => b.timeScore - a.timeScore);
+generateHigsoreTable();
+
+function generateHigsoreTable(){
+for (i = 0; i < scoresArray.length; i++){
+    if (i <= (topScoreList - 1)) {
+        highScoreList.append(createHTMLelement ("", "tr"));
+        highScoreList.children[i + 1].append(createHTMLelement ((i + 1 + "."), "td"));
+        highScoreList.children[i + 1].appendChild(createHTMLelement (scoresArray[i].player, "td"));
+        highScoreList.children[i + 1].appendChild(createHTMLelement (scoresArray[i].timeScore, "td"));
+        highScoreList.children[i + 1].appendChild(createHTMLelement (scoresArray[i].correctAnsw, "td"));
+        highScoreList.children[i + 1].appendChild(createHTMLelement (scoresArray[i].wrongAnsw, "td"));
+        }
+    }
+}
+
+function createHTMLelement(x,elem) {
+    let newElement = document.createElement(elem); 
+    newElement.textContent = x;
+    return newElement;
+}
+
+restartButton.addEventListener("click", function(){
+    localStorage.clear("scoresArray");
+    location.reload();
+});
+
+backToMainPage.addEventListener("click", function(){
+    location.href = "../../index.html";
+}); 
--- assets/js/main.js
+++ assets/js/main.js
@@ -3,22 +3,19 @@ let highscoreView = document.querySelector("#highscoreView");
 let timerView = document.querySelector("#timerView");
 let startInfo = document.querySelector("#startInfo");
 let allDone = document.querySelector("#allDone");
-let fillScore = document.querySelector("#fillScore");
+let finalScore = document.querySelector("#finalScore");
 let questionShow = document.querySelector("#questionShow");
 let answerButtons = document.querySelector("#answerButtons");
 let correctOrWrong = document.querySelector("#correctOrWrong");
 
-
-
 // Buttons
 let startButton = document.querySelector("#startButton");
 let initialsSubmitButton = document.querySelector("#initialsSubmitButton");
-let highScoreGoBackButton = document.querySelector("#highScoreGoBackButton");
-let restartButton = document.querySelector("#restartButton");
 
 // Text inputs
 let initials = document.querySelector("#initials");
 
+allDone.style.display = "none";
 let timer = 0;
 let secconds = 50;
 let questionTime = secconds;
@@ -33,30 +30,25 @@ let score = {
 }
 
 let scoresArray = [];
-// getFromStorage();
+scoresArray = JSON.parse(localStorage.getItem("scoresArray"));
+if (scoresArray === null) {
+     scoresArray = [];
+    }
 
-// function getFromStorage() {
-//     scoresArray = JSON.parse(localStorage.getItem("scoresArray"));
-//     // if (scoresArray !== null) {
-//     //     return;
-//     // document.querySelector(".message").textContent = lastGrade.student +  " received a/an " + lastGrade.grade
-//     // }
-// }
-
-
-// Quiz questions / answers
+// Quiz questions / answers - correct
 let quizArrayOriginal= [{'question': 'first', 'answers': ["one1", "two1", "three1", "fffffffffour1", 0]},
                         {'question': 'second', 'answers': ["ooooooooooone2", "two2", "three2", "four2", 1]},
                         {'question': 'third', 'answers': ["one3", "ttttttttttttwo3", "three3", "four3", 2]},
                         {'question': 'fourth', 'answers': ["one4", "two4", "tttttttttthree4", "four4", 3]},];
 
+// making a quiz array copy to work with
 let quizArray = [];
 quizArray.push.apply(quizArray, quizArrayOriginal);
          
-        
+
+// checking if the the pressed button is correct answer
 answerButtons.addEventListener("click", function(event) {
     let button = event.target;
-   
         if (button.matches("button")) {
             let state = button.getAttribute("data-state");
             if (state === "true") {
@@ -67,45 +59,51 @@ answerButtons.addEventListener("click", function(event) {
             } else {              
                 correctOrWrong.appendChild(createHTMLelement("Wrong", "p"));
                 // showCorrectWrongMessage();
-                questionTime -=10;
+                questionTime -= 10;
                 score.wrongAnsw++;
                 showCorrectWrongMessageDelay();
             }
         }
-  
 });
 
-// Event Listeners
-startButton.addEventListener("click", startTimer);
-
+// message corect or wrong answer
 function showCorrectWrongMessageDelay() {
-      correctOrWrong.style.borderTop = "1px solid #505050";
-      if (quizArray.length === 0) {
+    correctOrWrong.style.borderTop = "1px solid #505050";
+    if (quizArray.length === 0) {
         setTimeout(() =>{
             correctOrWrong.innerHTML = "";
             correctOrWrong.style.borderTop = "0px";
             ifNoMoreQuestions();
-        },1000);
+        },500);
     } else {
+        // let inactiveButtons = document.querySelectorAll(".amswButtons");
+        // for (i = 0; i < inactiveButtons.length; i++) {
+        //     inactiveButtons[i].disable = true;     
+        // }
+        // console.log(inactiveButtons);
         setTimeout(() =>{
             correctOrWrong.innerHTML = "";
             correctOrWrong.style.borderTop = "0px";
             startNextQuestion();
-        },1000);
+        },500);
     }
 }
-function startTimer () {
-    
+
+//start quiz button
+startButton.addEventListener("click", startTimer);
+
+function startTimer () {   
     startNextQuestion()
-    timerView.append(createHTMLelement(questionTime, "p"));
+    timerView.append(createHTMLelement(questionTime + " second(s) remaining", "p"));
     timer = setInterval(() => {
         questionTime--;
-        timerView.children[1].textContent = questionTime;
+        timerView.innerHTML = questionTime + " second(s) remaining";
         if (questionTime <= 0) {
-            clearInterval(timer);
+            ifNoMoreQuestions()
         } 
     }, 1000);
 }
+
 
 function createHTMLelement(x,elem) {
     let newElement = document.createElement(elem); 
@@ -117,14 +115,12 @@ function createRandomNr() {
     randomNr = Math.floor(Math.random() * quizArray.length);
 }
 
+// preapare questions and answers
 function setQuestion() {
-    console.log(quizArray.length, "ilgis")
     removingChildrens(questionShow);
     removingChildrens(answerButtons);
-    startInfo.style.display = "block";
+    startInfo.style.display = "none";
     allDone.style.display = "none";
-    fillScore.style.display = "none";
-    // questionShow.children[0].remove(); // h1 tag html TEMP
     questionShow.append(createHTMLelement(quizArray[randomNr].question, "p"));
     correctAnswer = quizArray[randomNr].answers[4];
     for (i = 0; i < quizArray[randomNr].answers.length - 1; i++) {
@@ -151,35 +147,34 @@ function startNextQuestion() {
     quizArray.splice(randomNr, 1);
 }
 
+//collecting and writing score results
 function writeScore () {
-    let initialsSubmitButton = document.querySelector("#initialsSubmitButton");
     initialsSubmitButton.addEventListener("click", function() {
-        score.player = document.getElementById("initials").value;
-        console.log(score.player);
-        console.log(score);
-        scoresArray.push(score);
-        localStorage.setItem("scoresArray", JSON.stringify(scoresArray));
-        initialsSubmitButton.display = true;
-       
+        if (document.getElementById("initials").value.trim() < 1){
+            allDone.appendChild(createHTMLelement("You need to enter name", "p"));
+        } else {
+            score.player = document.getElementById("initials").value;
+            scoresArray.push(score);
+            localStorage.setItem("scoresArray", JSON.stringify(scoresArray));
+            location.href = "./assets/html/highscore.html";
+        }     
     });
 }
 
+
 function ifNoMoreQuestions(){
-    // if (quizArray.length === 0) {
+    if (questionTime < 0) {
+        questionTime = 0;
+    } else {
         score.timeScore = questionTime;
-        writeScore ();
-        clearInterval(timer);
-        allDone.style.display = "block";
-        questionShow.style.display = "none";
-        answerButtons.style.display = "none";
-        correctOrWrong.style.display = "none";
-        removingChildrens(timerView);
-    // }
+    }
+    writeScore ();
+    clearInterval(timer);
+    finalScore.textContent = score.timeScore;
+    allDone.style.display = "block";
+    questionShow.style.display = "none";
+    answerButtons.style.display = "none";
+    correctOrWrong.style.display = "none";
+    // initials.value = "";
 }
 
-restartButton.addEventListener("click", function(){
-    quizArray = []
-    quizArray.push.apply(quizArray, quizArrayOriginal);
-    initialsSubmitButton.display = false;
-    questionTime = secconds;
-}) 
